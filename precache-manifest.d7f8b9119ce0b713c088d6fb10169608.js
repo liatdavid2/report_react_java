5fa2750082f09b2bdc6f7043915ab329f4c2c96d
liatdavid2 <liatdavid2@gmail.com>
2020-10-03 01:29:05 +0300

    start

---
+++ precache-manifest.d7f8b9119ce0b713c088d6fb10169608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9846d8de2d27167574e36a6dff25c951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc5b131bd4411b10ec8",
+    "url": "/static/css/main.f86e3e2f.chunk.css"
+  },
+  {
+    "revision": "355d2cd2d30537deb98c",
+    "url": "/static/js/2.0ec1d260.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ec1d260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc5b131bd4411b10ec8",
+    "url": "/static/js/main.1d687f98.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
